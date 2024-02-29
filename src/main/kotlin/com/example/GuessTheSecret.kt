d60d37e77a6fcb62a8bfd78f20b54eee51001cf8
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-02-29 19:58:38 +0000

    Cleanup unused imports

--- src/main/kotlin/com/example/GuessTheSecret.kt
+++ src/main/kotlin/com/example/GuessTheSecret.kt
@@ -1,12 +1,8 @@
 package com.example
 
-import org.http4k.core.Body
 import org.http4k.core.HttpHandler
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequest
-import org.http4k.format.Jackson.auto
-import org.http4k.lens.Path
-import org.http4k.lens.value
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
