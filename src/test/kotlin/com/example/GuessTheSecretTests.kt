32a0430b203628d1b25d987048a7df5729c36d89
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-03-01 13:10:41 +0000

    Ensure the journey test is run against a running app server

--- src/test/kotlin/com/example/GuessTheSecretTests.kt
+++ src/test/kotlin/com/example/GuessTheSecretTests.kt
@@ -1,12 +1,15 @@
 package com.example
 
+import org.http4k.core.Uri
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class GuessTheSecretTests {
 
-    val app = App(InMemoryGames())
-    val player = Player(app)
+    private val app = App(InMemoryGames()).asServer(SunHttp(0)).start()
+    private val player = Player(Uri.of("http://localhost:${app.port()}"))
 
     @Test
     fun `winning gameplay`() {
--- src/test/kotlin/com/example/Player.kt
+++ src/test/kotlin/com/example/Player.kt
@@ -1,19 +1,26 @@
 package com.example
 
+import org.http4k.client.JavaHttpClient
 import org.http4k.core.Body
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
+import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters.SetBaseUriFrom
 import org.http4k.format.Jackson.auto
 
-class Player(val http: HttpHandler) {
+class Player(baseUri: Uri) {
+
+    private val client = SetBaseUriFrom(baseUri).then(JavaHttpClient())
+
     fun startNewGame(): GameId {
-        val response = http(Request(Method.POST, "/games"))
+        val response = client(Request(POST, "/games"))
         return Body.auto<GameId>().toLens()(response)
     }
 
     fun hasWon(game: GameId): Boolean {
-        val response = http(Request(Method.GET, "/games/${game.value}"))
+        val response = client(Request(GET, "/games/${game.value}"))
         val details = Body.auto<GameDetails>().toLens()(response)
         return details.won
     }
