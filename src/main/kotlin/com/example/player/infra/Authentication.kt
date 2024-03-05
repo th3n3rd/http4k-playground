69863c92821d813cee59b430784b1ad533d58d5c
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-03-05 10:31:41 +0000

    Align structural convention for filtes/api handlers

--- src/main/kotlin/com/example/player/infra/Authentication.kt
+++ src/main/kotlin/com/example/player/infra/Authentication.kt
@@ -5,9 +5,11 @@ import com.example.player.PasswordEncoder
 import com.example.player.RegisteredPlayers
 import org.http4k.filter.ServerFilters.BasicAuth
 
-fun AuthenticatePlayer(players: RegisteredPlayers, passwordEncoder: PasswordEncoder) =
-    BasicAuth("example") {
-        players.findByUsername(it.user)
-            ?.let { player -> passwordEncoder(Password(it.password), player.password) }
-            ?: false
-    }
+object AuthenticatePlayer {
+    operator fun invoke(players: RegisteredPlayers, passwordEncoder: PasswordEncoder) =
+        BasicAuth("example") {
+            players.findByUsername(it.user)
+                ?.let { player -> passwordEncoder(Password(it.password), player.password) }
+                ?: false
+        }
+}
