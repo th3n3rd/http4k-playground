4bacacc6bff375a8571c332ca4e7687a248fb1f2
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-09 06:31:54 +0100

    Fix tracer bullet diagram by using inChildSpan when tracing operation triggered from the within the app

--- src/main/kotlin/com/example/gameplay/infra/TracingGames.kt
+++ src/main/kotlin/com/example/gameplay/infra/TracingGames.kt
@@ -6,16 +6,24 @@ import com.example.gameplay.GameId
 import com.example.gameplay.Games
 import com.example.player.PlayerId
 import org.http4k.events.Events
+import org.http4k.filter.ZipkinTracesStorage
+import org.http4k.filter.inChildSpan
 
 class TracingGames(val events: Events, val games: Games) : Games {
     override fun save(game: Game) {
-        events(DatabaseCall("games", "save"))
+        trace("save")
         games.save(game)
     }
 
     override fun findByIdAndPlayerId(id: GameId, playerId: PlayerId): Game? {
-        events(DatabaseCall("games", "find by id and player id"))
+        trace("find by id and player id")
         val existingGame = games.findByIdAndPlayerId(id, playerId)
         return existingGame
+    }
+
+    private fun trace(operation: String) {
+        ZipkinTracesStorage.THREAD_LOCAL.inChildSpan {
+            events(DatabaseCall("games", operation))
+        }
     }
 }
--- src/main/kotlin/com/example/player/infra/TracingRegisteredPlayers.kt
+++ src/main/kotlin/com/example/player/infra/TracingRegisteredPlayers.kt
@@ -4,21 +4,28 @@ import com.example.common.infra.DatabaseCall
 import com.example.player.RegisteredPlayer
 import com.example.player.RegisteredPlayers
 import org.http4k.events.Events
+import org.http4k.filter.ZipkinTracesStorage
+import org.http4k.filter.inChildSpan
 
 class TracingRegisteredPlayers(private val events: Events, private val players: RegisteredPlayers) : RegisteredPlayers {
     override fun save(player: RegisteredPlayer) {
-        events(DatabaseCall("players", "save"))
+        trace("save")
         players.save(player)
     }
 
     override fun findByUsername(username: String): RegisteredPlayer? {
-        events(DatabaseCall("players", "find by username"))
+        trace("find by username")
         return players.findByUsername(username)
     }
 
     override fun existByUsername(username: String): Boolean {
-        events(DatabaseCall("players", "exist by username"))
+        trace("exist by username")
         return players.existByUsername(username)
     }
 
+    private fun trace(operation: String) {
+        ZipkinTracesStorage.THREAD_LOCAL.inChildSpan {
+            events(DatabaseCall("players", operation))
+        }
+    }
 }
