d77493d125f3dbd8fd162c02b3fabc530db71329
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-09 06:32:36 +0100

    Add interaction diagram as part of the tracer bullet output

--- src/test/kotlin/com/example/common/infra/RecordTraces.kt
+++ src/test/kotlin/com/example/common/infra/RecordTraces.kt
@@ -1,15 +1,12 @@
 package com.example.common.infra
 
-import org.http4k.tracing.Actor
-import org.http4k.tracing.ActorResolver
+import org.http4k.tracing.*
 import org.http4k.tracing.ActorType.Database
 import org.http4k.tracing.ActorType.System
-import org.http4k.tracing.BiDirectional
-import org.http4k.tracing.TraceRenderPersistence
-import org.http4k.tracing.Tracer
 import org.http4k.tracing.junit.ReportingMode.Companion.Always
 import org.http4k.tracing.junit.TracerBulletEvents
 import org.http4k.tracing.persistence.FileSystem
+import org.http4k.tracing.renderer.PumlInteractionDiagram
 import org.http4k.tracing.renderer.PumlSequenceDiagram
 import org.http4k.tracing.tracer.HttpTracer
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -23,7 +20,7 @@ abstract class RecordTraces {
     @RegisterExtension
     val events = TracerBulletEvents(
         tracers = listOf(HttpTracer(resolveOrigin), DatabaseTracer(resolveOrigin)),
-        renderers = listOf(PumlSequenceDiagram),
+        renderers = listOf(PumlSequenceDiagram, PumlInteractionDiagram),
         traceRenderPersistence = TraceRenderPersistence.FileSystem(File("target/tracing")),
         reportingMode = Always
     )
