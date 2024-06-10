8d4e47a369e50358d0ef39aa4aaa91ce83d814e0
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-10 07:09:46 +0100

    Extract the game completed event in its own file

--- src/main/kotlin/com/example/gameplay/Game.kt
+++ src/main/kotlin/com/example/gameplay/Game.kt
@@ -4,7 +4,6 @@ import com.example.player.PlayerId
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Success
-import org.http4k.events.Event
 
 data class Game(
     val id: GameId = GameId(),
@@ -29,9 +28,3 @@ data class Game(
 
     data class Guess(val secret: String)
 }
-
-data class GameCompleted(
-    val gameId: GameId,
-    val playerId: PlayerId,
-    val attempts: Int
-): Event
