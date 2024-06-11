daddf83ff74abff54b09aafd7e0ba2a6d5bcbb2d
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-11 21:01:52 +0100

    Introduce a simple event bus

---
+++ src/main/kotlin/com/example/common/infra/EventsBus.kt
@@ -0,0 +1,11 @@
+package com.example.common.infra
+
+import org.http4k.events.Event
+import org.http4k.events.Events
+import kotlin.reflect.KClass
+
+interface EventsBus: Events {
+    operator fun <T: Event> invoke(eventType: KClass<T>, listener: (T) -> Unit)
+
+    companion object
+}
