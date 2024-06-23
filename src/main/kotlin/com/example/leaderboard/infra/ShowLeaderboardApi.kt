31a72b28d07e72fda272b05d08506140015eff6e
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-23 21:31:28 +0100

    Enhancing the openapi spec for the show leaderboard endpoint

--- src/main/kotlin/com/example/leaderboard/infra/ShowLeaderboardApi.kt
+++ src/main/kotlin/com/example/leaderboard/infra/ShowLeaderboardApi.kt
@@ -10,6 +10,7 @@ import org.http4k.core.Body
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Status.Companion.UNAUTHORIZED
 import org.http4k.core.with
 import org.http4k.format.Jackson.auto
 
@@ -18,7 +19,10 @@ object ShowLeaderboardApi {
         return "/leaderboard" meta {
             security = authentication
             summary = "Show leaderboard"
-            returning(OK to "Successful retrieval")
+            operationId = "showLeaderboard"
+
+            returning(OK, Response.leaderboard to Leaderboard(mapOf("alice" to 100, "bob" to 50)),"Successful retrieval")
+            returning(UNAUTHORIZED to "Not authenticated")
         } bindContract GET to { _ ->
             Response(OK).with(
                 Response.leaderboard of Leaderboard(
