a503982d02fc20742679146acd9422d039c7d5b8
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-06-25 06:32:40 +0100

    Pull out a contract test for the rankings repository

---
+++ src/test/kotlin/com/example/leaderboard/infra/InMemoryRankingsTests.kt
@@ -0,0 +1,25 @@
+package com.example.leaderboard.infra
+
+import com.example.leaderboard.Ranking
+import com.example.leaderboard.Rankings
+import com.example.leaderboard.RankingsContract
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+
+class InMemoryRankingsTests : RankingsContract {
+
+    override val rankings = Rankings.InMemory()
+
+    override fun given(ranking: Ranking): Ranking {
+        rankings.save(ranking)
+        return ranking
+    }
+
+    override fun haveBeenSaved() = Matcher<Ranking> { ranking ->
+        MatcherResult(
+            rankings.findAll().contains(ranking),
+            { "$ranking was not saved" },
+            { "$ranking should not have been saved" },
+        )
+    }
+}
--- src/test/kotlin/com/example/leaderboard/infra/TracingRankingsTests.kt
+++ src/test/kotlin/com/example/leaderboard/infra/TracingRankingsTests.kt
@@ -3,45 +3,56 @@ package com.example.leaderboard.infra
 import com.example.common.infra.DatabaseCall
 import com.example.leaderboard.Ranking
 import com.example.leaderboard.Rankings
+import com.example.leaderboard.RankingsContract
 import com.example.player.EncodedPassword
 import com.example.player.PlayerId
 import com.example.player.RegisteredPlayer
-import io.kotest.matchers.collections.shouldContainOnly
-import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.nulls.beNull
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNot
 import org.http4k.testing.RecordingEvents
 import org.junit.jupiter.api.Test
 
-class TracingRankingsTests {
+class TracingRankingsTests : RankingsContract {
 
     private val events = RecordingEvents()
-    private val rankings = Rankings.InMemory()
-    private val tracingRankings = TracingRankings(events, rankings)
+    private val inMemoryRankings = Rankings.InMemory()
 
     private val alice = RegisteredPlayer(PlayerId(), "alice", EncodedPassword("alice"))
     private val bob = RegisteredPlayer(PlayerId(), "bob", EncodedPassword("bob"))
     private val charlie = RegisteredPlayer(PlayerId(), "charlie", EncodedPassword("charlie"))
 
+    override val rankings = TracingRankings(events, inMemoryRankings)
+
+    override fun given(ranking: Ranking): Ranking {
+        inMemoryRankings.save(ranking)
+        return ranking
+    }
+
+    override fun haveBeenSaved()= Matcher<Ranking> { ranking ->
+        MatcherResult(
+            inMemoryRankings.findByPlayerId(ranking.playerId) != null,
+            { "$ranking was not saved" },
+            { "$ranking should not have been saved" },
+        )
+    }
+
     @Test
     fun `publish a new event when saving a ranking`() {
         val newRanking = Ranking(alice.id, alice.username, score = 100)
 
-        tracingRankings.save(newRanking)
+        rankings.save(newRanking)
 
-        rankings.findByPlayerId(alice.id) shouldNot beNull()
         events.toList() shouldBe listOf(DatabaseCall("rankings", "save"))
     }
 
     @Test
     fun `publish a new event when retrieving an existing ranking by player id`() {
         val existingRanking = Ranking(alice.id, alice.username, score = 100)
-        rankings.save(existingRanking)
+        inMemoryRankings.save(existingRanking)
 
-        val retrievedRanking = tracingRankings.findByPlayerId(existingRanking.playerId)
+        rankings.findByPlayerId(existingRanking.playerId)
 
-        retrievedRanking shouldBe existingRanking
         events.toList() shouldBe listOf(DatabaseCall("rankings", "find by player id"))
     }
 
@@ -52,11 +63,10 @@ class TracingRankingsTests {
             Ranking(bob.id, bob.username, score = 200),
             Ranking(charlie.id, charlie.username, score = 300),
         )
-        existingRankings.forEach { rankings.save(it) }
+        existingRankings.forEach { inMemoryRankings.save(it) }
 
-        val retrievedRankings = tracingRankings.findAll()
+        rankings.findAll()
 
-        retrievedRankings shouldContainOnly existingRankings
         events.toList() shouldBe listOf(DatabaseCall("rankings", "find all"))
     }
 }
