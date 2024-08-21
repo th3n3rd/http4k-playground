5fae5e1e085c5a21ca9f2dbc4e620e4a4c996713
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-08-21 10:40:38 +0100

    Switch the game details api endpoint test to use approval testing

--- src/test/kotlin/com/example/gameplay/infra/GetGameDetailsApiTests.kt
+++ src/test/kotlin/com/example/gameplay/infra/GetGameDetailsApiTests.kt
@@ -1,25 +1,24 @@
 package com.example.gameplay.infra
 
-import com.example.player.infra.PlayerRequestContext.withPlayerId
-import com.example.player.infra.authenticatedAs
 import com.example.gameplay.Game
+import com.example.gameplay.GameId
 import com.example.gameplay.Games
 import com.example.gameplay.GetGameDetails
 import com.example.player.PlayerId
-import io.kotest.assertions.json.schema.jsonSchema
-import io.kotest.assertions.json.schema.obj
-import io.kotest.assertions.json.schema.shouldMatchSchema
-import io.kotest.assertions.json.shouldEqualSpecifiedJson
-import io.kotest.common.ExperimentalKotest
+import com.example.player.infra.PlayerRequestContext.withPlayerId
+import com.example.player.infra.authenticatedAs
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.beUUID
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
+import org.http4k.kotest.shouldHaveStatus
+import org.http4k.testing.Approver
+import org.http4k.testing.JsonApprovalTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-@OptIn(ExperimentalKotest::class)
+@ExtendWith(JsonApprovalTest::class)
 class GetGameDetailsApiTests {
 
     private val anotherPlayer = PlayerId()
@@ -30,34 +29,19 @@ class GetGameDetailsApiTests {
         .authenticatedAs(authenticatedPlayerId)
 
     @Test
-    fun `present the details of an existing game`() {
-        val existingGame = Game(playerId = authenticatedPlayerId, secret = "dont-care")
+    fun `present the details of an existing game`(approver: Approver) {
+        val existingGame = Game(id = GameId.Placeholder, playerId = authenticatedPlayerId, secret = "dont-care")
         games.save(existingGame)
 
         val response = api(Request(GET, "/games/${existingGame.id.value}"))
 
-        with(response) {
-            status shouldBe OK
-            bodyString() shouldMatchSchema jsonSchema {
-                obj {
-                    string("id") { beUUID() }
-                    string("hint")
-                    boolean("won")
-                    additionalProperties = false
-                }
-            }
-            bodyString() shouldEqualSpecifiedJson """
-            {
-                "hint": "_________",
-                "won": false
-            }
-            """.trimIndent()
-        }
+        response shouldHaveStatus OK
+        approver.assertApproved(response)
     }
 
     @Test
     fun `fails when the game does not exist for the authenticated player`() {
-        val existingGame = Game(playerId = anotherPlayer)
+        val existingGame = Game(id = GameId.Placeholder, playerId = anotherPlayer)
         games.save(existingGame)
 
         val response = api(Request(GET, "/games/${existingGame.id.value}"))
